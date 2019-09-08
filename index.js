91e210b2b12e0ee4687e7f5d2cc4a332801e933a
mashupmark <mashupmark@kabelbw.de>
2019-09-08 10:41:58 +0200

    test with multiple events

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const { writeFileSync } = require('fs')
 const ics = require('ics')
  
-ics.createEvent({
+ics.createEvents([{
   title: 'Test',
   description: 'Ein Test Event',
   start: [2019, 9, 8, 18, 0],
   startInputType: "local",
   duration: { minutes: 60 }
-}, (error, value) => {
+},
+{
+    title: 'Test2',
+    description: 'Ein weiteres Test Event',
+    start: [2019, 9, 8, 20, 0],
+    startInputType: "local",
+    duration: { minutes: 30 }
+  }
+], (error, value) => {
   if (error) {
     console.log(error)
   }

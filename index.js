f627264815ddfa668420ac50b13a91beadc8af96
mashupmark <mashupmark@kabelbw.de>
2019-09-08 12:24:14 +0200

    Transforming the api data to ics event

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
 const { writeFileSync } = require('fs')
 const ics = require('ics')
 const moment = require('moment')
- 
-ics.createEvents([{
-  title: 'Test',
-  description: 'Ein Test Event',
-  start: [2019, 9, 8, 18, 0],
-  startInputType: "local",
-  duration: { minutes: 60 }
-},
-{
-    title: 'Test2',
-    description: 'Ein weiteres Test Event',
-    start: [2019, 9, 8, 20, 0],
-    startInputType: "local",
-    alarms: [],
-    duration: { minutes: 30 }
-  }
-], (error, value) => {
-  if (error) {
-    console.log(error)
-  }
- 
-  console.log(value)
+const getJson = require('get-json')
+
+getJson('https://stuv-mosbach.de/survival/api.php?action=getLectures&course=INF18B')
+  .then(function(response) {
+
+    var events = response.filter(function(event) {
+      return event.over === false && event.name !== "Studientag";
+    });
+
+    var entries = [];
+    events.forEach(function(event) {
+      var startDate = event.start_date.split('.');
+      var startTime = event.start_time.split(':');
+
+      var entry = {
+        title: event.name,
+        start: [Number(startDate[2]), Number(startDate[1]), Number(startDate[0]), Number(startTime[0]), Number(startTime[1])],
+        duration: { minutes: event.duration },
+        location: event.location,
+        organizer: event.lecturer
+      }
+
+      entries.push(entry);
+    });
+
+    ics.createEvents(entries, (error, value) => {
+      if (error) {
+        console.log(error)
+      }
+    });
+
+    push();
+    console.log(entries);
+  }).catch(function(error) {
+    console.log(error);
+  });
+
+function push() {
   writeFileSync(`${__dirname}/events.ics`, value)
 
   console.log("Pushing to GitHub...");
-  
+
   require('simple-git')()
     .add("./events.ics")
     .commit("Update " + moment().format('YYYY-MM-DD:hh:mm:ss'))
     .push(['-u', 'origin', 'master'], () => console.log("Done"));
-})
+}

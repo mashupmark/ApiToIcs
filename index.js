fdb78951f67be9bc78fbbaa63789408bfcbf717c
mashupmark <mashupmark@kabelbw.de>
2019-09-26 16:07:44 +0200

    Speiseplan hinzugefügt (experimentell)

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const { writeFileSync } = require('fs')
+const writeFileSync = require('fs')
 const ics = require('ics')
 const moment = require('moment')
 const getJson = require('get-json')
+const crawler = require('crawler-request')
 
 getJson('https://stuv-mosbach.de/survival/api.php?action=getLectures&course=INF18B')
-  .then(function (response) {
+  .then(async function (response) {
 
     var events = response.filter(function (event) {
       return event.over === false && event.name !== "Studientag";
@@ -38,17 +39,20 @@ getJson('https://stuv-mosbach.de/survival/api.php?action=getLectures&course=INF1
         last.start[2] === current.start[2] &&
         last.start[3] === current.start[3] &&
         last.start[4] === current.start[4]) {
-          entries[i-1].location = entries[i-1].location + "; " + current.location;
-          entries.splice(i, 1);
-          i--;
-        }
+        entries[i - 1].location = entries[i - 1].location + "; " + current.location;
+        entries.splice(i, 1);
+        i--;
+      }
     }
 
+    let meals = await getMeals();
+    addMeals(meals, entries);
     ics.createEvents(entries, (error, value) => {
       if (error) {
         console.log(error)
       }
-      
+
+      console.log(entries);
       push(value);
     });
   }).catch(function (error) {
@@ -64,4 +68,53 @@ function push(value) {
     .add("./events.ics")
     .commit("Update " + moment().format('YYYY-MM-DD:hh:mm:ss'))
     .push(['-u', 'origin', 'master'], () => console.log("Done"));
+}
+
+async function getMeals() {
+  var meals = new Array(5);
+
+  meals = await crawler("https://www.studentenwerk.uni-heidelberg.de/sites/default/files/download/pdf/sp-mos-mensa-aktuell.pdf").then(function (response) {
+
+    var chars = response.text.split('\n');
+
+    for (let index = 0; index < chars.length; index++) {
+      const element = chars[index];
+      switch (element) {
+        case 'Montag ':
+          meals[0] = chars[index + 1] + "\n" + chars[index + 2] + "\n" + chars[index + 3] + "\n" + chars[index + 4];
+          break;
+        case 'Dienstag ':
+          meals[1] = chars[index + 1] + "\n" + chars[index + 2] + "\n" + chars[index + 3] + "\n" + chars[index + 4];
+          break;
+        case 'Mittwoch ':
+          meals[2] = chars[index + 1] + "\n" + chars[index + 2] + "\n" + chars[index + 3] + "\n" + chars[index + 4];
+          break;
+        case 'Donnerstag ':
+          meals[3] = chars[index + 1] + "\n" + chars[index + 2] + "\n" + chars[index + 3] + "\n" + chars[index + 4];
+          break;
+        case 'Freitag ':
+          meals[4] = chars[index + 1] + "\n" + chars[index + 2] + "\n" + chars[index + 3] + "\n" + chars[index + 4];
+          break;
+        default:
+          break;
+      }
+    }
+    return meals;
+  });
+
+  return meals;
+}
+
+function addMeals(meals, entries) {
+  var currentDay = moment().isoWeekday();
+    for (let index = 0; index < (6 - currentDay); index++) {
+      const first = entries[index];
+      const second = entries[index + 1];
+
+      if(first.start[0] === second.start[0] &&
+        first.start[1] === second.start[1] &&
+        first.start[2] === second.start[2]) {
+          entries[index].description += "\n\nMittagessen:\n" + meals[currentDay + index];
+      }
+    }
 }
